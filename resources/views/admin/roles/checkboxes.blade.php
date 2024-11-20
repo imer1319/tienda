e9579fe20935e761308873f8f6037a8177694d09
imer1319 <imer1319nd@gmail.com>
2024-11-19 23:51:31 -0300

    roles diferente a cliente

--- resources/views/admin/roles/checkboxes.blade.php
+++ resources/views/admin/roles/checkboxes.blade.php
@@ -1,9 +1,11 @@
 @foreach ($roles as $role)
-    <div class="form-check">
-        <input class="form-check-input" type="radio" name="roles[]" id="{{ $role->name }}" value="{{ $role->name }}"
-               {{ $user->roles->contains($role->id) ? 'checked' : '' }}>
-        <label class="form-check-label" for="{{ $role->name }}">
-            {{ $role->display_name }}
-        </label>
-    </div>
+    @if ($role->name != 'Cliente')
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="roles[]" id="{{ $role->name }}"
+                value="{{ $role->name }}" {{ $user->roles->contains($role->id) ? 'checked' : '' }}>
+            <label class="form-check-label" for="{{ $role->name }}">
+                {{ $role->display_name }}
+            </label>
+        </div>
+    @endif
 @endforeach
